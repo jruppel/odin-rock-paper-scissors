2bfe8457afc7cf43d24690babb7725d764166684
Jonathan Ruppel <jonathan_ruppel@yahoo.com>
2025-07-27 17:18:49 -0500

    adding human choice

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+// randomly generates rock, paper, or scissors for the computer choice 
 function getComputerChoice() {
     let choice_num=Math.random()
     if(choice_num>=0 && choice_num<=.33) {
-        
         return "rock"
     }
     else if(choice_num>.33 && choice_num<=.66) {
@@ -12,4 +12,10 @@ function getComputerChoice() {
     }
 }
 
+//prompts user for rock, paper, or scissors choice
+function getHumanChoice() {
+    return prompt("Rock, paper, or scissors?")
+}
+
 console.log(getComputerChoice())
+console.log(getHumanChoice())
