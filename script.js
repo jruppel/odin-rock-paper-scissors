ebb65c46ae46a9bc925cbfe58e6aea14db7f3539
Jonathan Ruppel <jonathan_ruppel@yahoo.com>
2025-07-27 16:37:54 -0500

    adding computer choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let choice_num=Math.random()
+    if(choice_num>=0 && choice_num<=.33) {
+        
+        return "rock"
+    }
+    else if(choice_num>.33 && choice_num<=.66) {
+        return "paper"
+    }
+    else if(choice_num>.66 && choice_num<=1) {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
